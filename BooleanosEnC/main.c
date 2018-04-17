b46bc2ef215e1bd65abfdc14877094c0e629f3c5
rchampa <ricardo.champa@gmail.com>
2018-04-17 17:05:08 +0200

    testing booleans in C

--- BooleanosEnC/main.c
+++ BooleanosEnC/main.c
@@ -8,14 +8,29 @@
 
 #include <stdio.h>
 
+int esPar(int n){
+    return n%2==0;
+}
+
 int esMultiplo21(int n){
     return n%21==0;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("true %d\n",esMultiplo21(21));
-    printf("false = %d\n",esMultiplo21(22));
+
+    int x = esPar(2);
+    int y = esPar(3);
+    int z = esPar(4);
+    printf("x is even? %d\n",x);
+    printf("y is even? %d\n",y);
+    printf("z is even? %d\n",z);
+    
+    printf("\n");
+
+    int k = esMultiplo21(21);
+    int j = esMultiplo21(2);
+    printf("k is a multiple of 21?  %d\n",k);
+    printf("j is a multiple of 21? %d\n",j);
     
     return 0;
 }
