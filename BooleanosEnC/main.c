aad47b59a9fb755a0f1131d6d4e4f4c57fdb4244
rchampa <ricardo.champa@gmail.com>
2018-04-17 16:52:56 +0200

    Initial Commit

---
+++ BooleanosEnC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BooleanosEnC
+//
+//  Created by Ricardo Champa on 17/04/2018.
+//  Copyright © 2018 Ricardo Champa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
